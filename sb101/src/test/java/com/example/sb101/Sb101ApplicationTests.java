25019472aaed827a89f1be00c8c596e865de1ec6
wooseokoh <allsafe1206@gmail.com>
2022-09-28 21:49:55 +0900

    01.sb101 project create and swagger setting complete

---
+++ sb101/src/test/java/com/example/sb101/Sb101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
