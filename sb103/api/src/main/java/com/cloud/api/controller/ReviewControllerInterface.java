0a56677a7adc17920d89c096d0ac1554fc19bff6
wooseokoh <allsafe1206@gmail.com>
2022-10-15 01:06:19 +0900

    03.sb103 api controller class add

---
+++ sb103/api/src/main/java/com/cloud/api/controller/ReviewControllerInterface.java
@@ -0,0 +1,18 @@
+package com.cloud.api.controller;
+
+import com.cloud.api.dto.Review;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface ReviewControllerInterface {
+
+    @PostMapping(value = "/review", consumes = "application/json", produces = "application/json")
+    Review createReview(@RequestBody Review body);
+
+    @GetMapping(value = "/review", produces = "application/json"    )
+    List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+
+    @DeleteMapping(value = "/review")
+    void deleteReviews(@RequestParam(value = "productId", required = true)  int productId);
+}
