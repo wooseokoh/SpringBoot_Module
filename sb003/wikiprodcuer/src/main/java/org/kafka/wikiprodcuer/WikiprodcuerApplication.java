e129a4f63dcfa531af939454f547daab71759c64
wooseokoh <allsafe1206@gmail.com>
2022-11-04 21:44:36 +0900

    sb003 producer run

--- sb003/wikiprodcuer/src/main/java/org/kafka/wikiprodcuer/WikiprodcuerApplication.java
+++ sb003/wikiprodcuer/src/main/java/org/kafka/wikiprodcuer/WikiprodcuerApplication.java
@@ -1,13 +1,24 @@
 package org.kafka.wikiprodcuer;
 
+import org.kafka.wikiprodcuer.config.WikimediaChangesProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WikiprodcuerApplication {
+public class WikiprodcuerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(WikiprodcuerApplication.class, args);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 
 }
