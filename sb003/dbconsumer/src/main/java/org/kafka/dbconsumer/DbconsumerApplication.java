27690f6d345b9ec77837593d28dab5cc5b7c540f
wooseokoh <allsafe1206@gmail.com>
2022-11-04 22:19:59 +0900

    sb003 create consumer proj

---
+++ sb003/dbconsumer/src/main/java/org/kafka/dbconsumer/DbconsumerApplication.java
@@ -0,0 +1,13 @@
+package org.kafka.dbconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbconsumerApplication.class, args);
+    }
+
+}
---
+++ sb003/dbconsumer/src/test/java/org/kafka/dbconsumer/DbconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kafka.dbconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
