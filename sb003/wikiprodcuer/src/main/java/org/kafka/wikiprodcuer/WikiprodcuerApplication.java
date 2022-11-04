d0474056a0a4633ae7443f83dd4e9a86155cec3a
wooseokoh <allsafe1206@gmail.com>
2022-11-04 16:25:37 +0900

    sb003 kafka ex proj configure producer and create a topic

---
+++ sb003/wikiprodcuer/src/main/java/org/kafka/wikiprodcuer/WikiprodcuerApplication.java
@@ -0,0 +1,13 @@
+package org.kafka.wikiprodcuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiprodcuerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiprodcuerApplication.class, args);
+    }
+
+}
