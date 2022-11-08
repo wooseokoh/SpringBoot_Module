6cb1f2c9d14f6710646406a93404c0babe6c4a04
wooseokoh <allsafe1206@gmail.com>
2022-11-08 22:41:05 +0900

    12.sb112 create sb112 prj init commit

---
+++ sb112/src/main/java/com/example/sb112/Sb112Application.java
@@ -0,0 +1,13 @@
+package com.example.sb112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb112Application.class, args);
+    }
+
+}
---
+++ sb112/src/test/java/com/example/sb112/Sb112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
