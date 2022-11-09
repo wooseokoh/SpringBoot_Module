b55ed505cd6e35aa756beb2879282d7f14a3e57d
wooseokoh <allsafe1206@gmail.com>
2022-11-09 23:33:28 +0900

    12.sb112 mariadb setup

--- sb112/src/test/java/com/example/sb112/Sb112ApplicationTests.java
+++ sb112/src/test/java/com/example/sb112/Sb112ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.sb112;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb112ApplicationTests {
 
     @Test
