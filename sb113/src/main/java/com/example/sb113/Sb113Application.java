ce7e72d563fbde8abaaa229ea103d8eec7778f7e
wooseokoh <allsafe1206@gmail.com>
2022-11-22 23:24:43 +0900

    13.sb113 springboot + react init

---
+++ sb113/src/main/java/com/example/sb113/Sb113Application.java
@@ -0,0 +1,13 @@
+package com.example.sb113;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb113Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb113Application.class, args);
+    }
+
+}
